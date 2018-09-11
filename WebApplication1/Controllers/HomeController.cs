38193718649c1e8b9bca3455eadf68c2ff4bcad4
Katya Leizarovich <Katya.Leizarovich@jetbrains.com>
2018-09-11 17:08:07 +0300

    red code

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
-  public class HomeController : Controller
+  public class HomeControllerNew : Controller
   {
     public IActionResult Index()
     {
